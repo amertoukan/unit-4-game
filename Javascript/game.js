1c255a43c746c3bd662dc2ea8098ba6fe9436143
John Doe <toukan.amer@gmail.com>
2018-06-13 16:08:09 -0400

    #003: html ready

---
+++ Javascript/game.js
@@ -0,0 +1,16 @@
+//Generate random number 
+var randomNumber = Math.round ( Math.random * (120 - 19 + 1) ) + 19;
+//Options(Diamonds)
+//When clicked function 
+
+//Option 1
+var RedDiamond;
+
+//Option 2
+var GreenDiamond;
+
+//Option 3 
+var YellowDiamond; 
+
+//Option 4
+var BlackDiamond; 
