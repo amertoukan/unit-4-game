d573ffb7c2da81579f4202de571e2385f75698d4
John Doe <toukan.amer@gmail.com>
2018-06-13 17:13:51 -0400

    #004:images folder/html issue fixed/ javascript built

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,16 +1,74 @@
-//Generate random number 
-var randomNumber = Math.round ( Math.random * (120 - 19 + 1) ) + 19;
-//Options(Diamonds)
-//When clicked function 
+$(document).ready(function(){
+    //Display inital score to zero
+    console.log("Resetting numbers..");
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+    var red = 0;
+    var green = 0;
+    var yellow = 0;
+    var black = 0; 
+    var randomNum = 0; 
+    console.log("Numbers reset");
+$("#score").html(totalScore);
 
-//Option 1
-var RedDiamond;
+//Generating randomNum
+const generateRandomNum = function(){
+    let random = Math.floor(Math.random()*120+19);
+    randomNum += random;
+    return random;
+}
+$("#randomNum").html(generateRandomNum());
 
-//Option 2
-var GreenDiamond;
+//Generating random crystal numbers function
+const generateCrystalNum = function (){
+    let random = Math.floor(Math.random()*12+1);
+    return random;
+}
+//Generate random crystal function to each option 
+const CrystNum = function (){
+red += generateCrystalNum();
+green += generateCrystalNum();
+yellow += generateCrystalNum();
+black += generateCrystalNum() ;
+}
+CrystNum();
 
-//Option 3 
-var YellowDiamond; 
+//Assign button clicked to the right crystal 
+$("button").on("click",function(){
+    if (this.id === "red"){
+        totalScore += red;
+    } else if (this.id === "green"){
+        totalScore += green; 
+    } else if (this.id === "yellow"){
+        totalScore+= yellow;
+    } else if (this.id === "black"){
+        totalScore += black;
+    }
+});
+const winOrLose = function(){ 
+    if (totalScore === randomNum){
+        wins += 1
+        $("#win").html(wins);
+        alert ("Winner Winner Chicken Dinner!");
+        reset();
+    } else if (totalScore > randomNum){
+        losses += 1; 
+        $("#loss").html(losses);
+        alert ("Oh no! You lost, try again.")
+        reset();
+    }
+}
+//Resetting all values to zero 
+const reset = function () {
+    totalScore = 0;
+    red = 0;
+    green = 0;
+    yellow = 0; 
+    black = 0;
+    randomNum = 0;
+   CrystNum();
+   $("#randomNum").html(randomNum += generateRandomNum());
+}
 
-//Option 4
-var BlackDiamond; 
+})
