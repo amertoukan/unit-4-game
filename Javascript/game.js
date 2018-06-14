fc982977f5e3fe2b2340465feec513bd9c53c0c2
John Doe <toukan.amer@gmail.com>
2018-06-13 20:31:14 -0400

    #005: javascript,css,html functional

--- Javascript/game.js
+++ Javascript/game.js
@@ -4,17 +4,17 @@ $(document).ready(function(){
     var wins = 0;
     var losses = 0;
     var totalScore = 0;
-    var red = 0;
-    var green = 0;
+    var red = 0; 
+    var blue = 0;
     var yellow = 0;
-    var black = 0; 
+    var green = 0; 
     var randomNum = 0; 
     console.log("Numbers reset");
 $("#score").html(totalScore);
 
 //Generating randomNum
 const generateRandomNum = function(){
-    let random = Math.floor(Math.random()*120+19);
+    let random = Math.floor(Math.random()*(120 - 19) + 19);
     randomNum += random;
     return random;
 }
@@ -22,33 +22,35 @@ $("#randomNum").html(generateRandomNum());
 
 //Generating random crystal numbers function
 const generateCrystalNum = function (){
-    let random = Math.floor(Math.random()*12+1);
+    let random = Math.floor (Math.random() * (12 - 1) +1 );
     return random;
 }
 //Generate random crystal function to each option 
-const CrystNum = function (){
+const crystNum = function (){
 red += generateCrystalNum();
-green += generateCrystalNum();
+blue += generateCrystalNum();
 yellow += generateCrystalNum();
-black += generateCrystalNum() ;
+green += generateCrystalNum() ;
 }
-CrystNum();
-
+crystNum();
 //Assign button clicked to the right crystal 
 $("button").on("click",function(){
     if (this.id === "red"){
         totalScore += red;
-    } else if (this.id === "green"){
-        totalScore += green; 
+    } else if (this.id === "blue"){
+        totalScore += blue; 
     } else if (this.id === "yellow"){
         totalScore+= yellow;
-    } else if (this.id === "black"){
-        totalScore += black;
+    } else if (this.id === "green"){
+        totalScore += green;
     }
+//setting total score to the value pressed 
+$("#score").html(totalScore);
+winOrLose();
 });
 const winOrLose = function(){ 
     if (totalScore === randomNum){
-        wins += 1
+        wins += 1;
         $("#win").html(wins);
         alert ("Winner Winner Chicken Dinner!");
         reset();
@@ -61,14 +63,14 @@ const winOrLose = function(){
 }
 //Resetting all values to zero 
 const reset = function () {
-    totalScore = 0;
-    red = 0;
-    green = 0;
-    yellow = 0; 
-    black = 0;
-    randomNum = 0;
-   CrystNum();
-   $("#randomNum").html(randomNum += generateRandomNum());
+totalScore = 0;
+red = 0;
+blue = 0;
+yellow = 0; 
+green = 0;
+randomNum = 0;
+crystNum();
+$("#randomNum").html(randomNum += generateRandomNum());
 }
 
-})
+});
